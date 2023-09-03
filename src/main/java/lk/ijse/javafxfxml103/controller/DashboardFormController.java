b63432415ebaa7c9ebd6caee04d139d54e83b538
danu <danujagreru@gmail.com>
2023-09-03 15:12:12 +0530

    create dashboard

---
+++ src/main/java/lk/ijse/javafxfxml103/controller/DashboardFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.javafxfxml103.controller;
+
+/*
+    @author DanujaV
+    @created 9/3/23 - 3:07 PM   
+*/
+
+import javafx.event.ActionEvent;
+
+public class DashboardFormController {
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+        //open customer form
+    }
+}
