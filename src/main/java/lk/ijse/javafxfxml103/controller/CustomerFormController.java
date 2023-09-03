1af8f772b77b5b0203eb1c390d4f7d18fc7c82fb
danu <danujagreru@gmail.com>
2023-09-03 15:19:35 +0530

    navigate customer form done

---
+++ src/main/java/lk/ijse/javafxfxml103/controller/CustomerFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.javafxfxml103.controller;
+
+/*
+    @author DanujaV
+    @created 9/3/23 - 3:13 PM   
+*/
+
+public class CustomerFormController {
+}
--- src/main/java/lk/ijse/javafxfxml103/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/javafxfxml103/controller/DashboardFormController.java
@@ -6,9 +6,24 @@ package lk.ijse.javafxfxml103.controller;
 */
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
-    public void btnCustomerOnAction(ActionEvent actionEvent) {
-        //open customer form
+    public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = new Stage();
+        stage.setTitle("Customer Form");
+
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
