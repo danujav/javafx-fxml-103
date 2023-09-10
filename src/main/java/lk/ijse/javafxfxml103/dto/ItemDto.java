066f80f2d39c6406227b59e7e5366b1c0892b252
danu <danujagreru@gmail.com>
2023-09-10 13:28:28 +0530

    add lombok to the project

--- src/main/java/lk/ijse/javafxfxml103/dto/ItemDto.java
+++ src/main/java/lk/ijse/javafxfxml103/dto/ItemDto.java
@@ -5,61 +5,17 @@ package lk.ijse.javafxfxml103.dto;
     @created 9/10/23 - 12:33 PM   
 */
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+/*@Setter
+@Getter
+@ToString*/
 public class ItemDto {
     private String code;
     private String description;
     private double unitPrice;
     private int qtyOnHand;
-
-    public ItemDto() {
-    }
-
-    public ItemDto(String code, String description, double unitPrice, int qtyOnHand) {
-        this.code = code;
-        this.description = description;
-        this.unitPrice = unitPrice;
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public int getQtyOnHand() {
-        return qtyOnHand;
-    }
-
-    public void setQtyOnHand(int qtyOnHand) {
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    @Override
-    public String toString() {
-        return "ItemDto{" +
-                "code='" + code + '\'' +
-                ", description='" + description + '\'' +
-                ", unitPrice=" + unitPrice +
-                ", qtyOnHand=" + qtyOnHand +
-                '}';
-    }
 }
